c89852ed63dcc04fbbe4e974845a97a760625bf1
Irina Sirbu <Irina.Sirbu@endava.com>
2017-07-12 14:30:00 +0300

    new app

--- src/test/java/com/github/messenger4j/quickstart/boot/ApplicationTests.java
+++ src/test/java/com/github/messenger4j/quickstart/boot/ApplicationTests.java
@@ -1,4 +1,4 @@
-package me.aboullaite;
+package com.github.messenger4j.quickstart.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringDocBotApplicationTests {
+public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
